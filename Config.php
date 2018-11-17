25b6858882edc7ca3f40fb2c4bd529695a72996d
Domenico Giambra <zorinik@gmail.com>
2018-11-17 13:43:01 +0100

    "if not exists"

--- Config.php
+++ Config.php
@@ -30,7 +30,7 @@ class Config extends Module_Config
 	 */
 	public function install(array $data = []): bool
 	{
-		$this->model->_Db->query('CREATE TABLE `model_notification_rules` (
+		$this->model->_Db->query('CREATE TABLE IF NOT EXISTS `model_notification_rules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_idx` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `user` int(11) NOT NULL,
@@ -41,7 +41,7 @@ class Config extends Module_Config
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
 
-		$this->model->_Db->query('CREATE TABLE `model_notifications` (
+		$this->model->_Db->query('CREATE TABLE IF NOT EXISTS `model_notifications` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `short_text` text COLLATE utf8_unicode_ci,
@@ -52,7 +52,7 @@ class Config extends Module_Config
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
 
-		$this->model->_Db->query('CREATE TABLE `model_notifications_recipients` (
+		$this->model->_Db->query('CREATE TABLE IF NOT EXISTS `model_notifications_recipients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `notification` int(11) NOT NULL,
   `user_idx` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
