54d3213ccbbaafd09cf58cb1f4b010500c48ffe2
Domenico <zorinik@gmail.com>
2025-10-24 20:48:46 +0200

    adattamento a nuovo router

---
+++ Providers/RouterProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Model\Notifications\Providers;
+
+use Model\Router\AbstractRouterProvider;
+
+class RouterProvider extends AbstractRouterProvider
+{
+	public static function getRoutes(): array
+	{
+		return [
+			[
+				'pattern' => '/model-notifications',
+				'controller' => 'ModelNotifications',
+			],
+		];
+	}
+}
